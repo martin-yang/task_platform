0713838b69e0b7873ae7b2aba0f093dd0ef54e81
yangjunfeng <408290909@qq.com>
2015-01-10 20:50:39 +0800

    c创建根据时间获取任务的方法

--- app/models/date_record.rb
+++ app/models/date_record.rb
@@ -1,4 +1,9 @@
 class DateRecord < ActiveRecord::Base
   validates :year, :month, :day, presence: true
   has_many :tasks, class_name: "Task"
+
+  def self.take_tasks nt
+    date_record = self.find_or_create_by(year: nt.year, month: nt.month, day: nt.day)
+    date_record.tasks
+  end
 end
