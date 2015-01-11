d992488e3b9e63b63e2a7c78284e124e22d9ae24
yangjunfeng <408290909@qq.com>
2015-01-11 22:09:23 +0800

    加上sidekiq配置以及定时任务

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,19 +2,19 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.at '21:40:00' do
+scheduler.at '22:05:00' do
   p "------new day start----"
   emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
   GoodMorning.perform_async(emails)
 end
 
-scheduler.at '21:45:00' do
+scheduler.at '22:06:00' do
   p "------noon-------"
   emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
   GoodNoon.perform_async(emails)
 end
 
-scheduler.at '21:50:00' do
+scheduler.at '22:07:00' do
   p "------after noon------"
   emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
   GoodAfterNoon.perform_async(emails)
