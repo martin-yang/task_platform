fec1161b6f36cd159c918c2a113d7d1435de0c16
yangjunfeng <408290909@qq.com>
2015-01-11 21:02:27 +0800

    加入根据时间获取用户

--- spec/models/date_record_spec.rb
+++ spec/models/date_record_spec.rb
@@ -4,9 +4,15 @@ require 'rails_helper'
 describe DateRecord, :type => :model do
   before do
     FactoryGirl.create(:task)
+    FactoryGirl.create(:join_user)
   end
 
   it "take tasks by now time" do
     expect(DateRecord.take_tasks(Time.now).first).to eq(Task.first)
+  end
+
+  it "take had tasks for user by now time" do
+    expect(User.all.size).to eq(2)
+    expect(TakeTaskPerson.execute(Time.now).size).to eq(1)
   end
 end
