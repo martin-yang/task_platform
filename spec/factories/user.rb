e8405fcb5ded5b83a4cbe350d026c2a0fa40dc49
yangjunfeng <408290909@qq.com>
2015-01-11 20:57:02 +0800

    加入新用户

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,12 @@
 FactoryGirl.define do
   factory :user, class: User do
-    email "408290909@qq.com"
+    email "test@qq.com"
+    password "12345678"
+    password_confirmation "12345678"
+  end
+
+  factory :join_user, class: User do
+    email "join_test@qq.com"
     password "12345678"
     password_confirmation "12345678"
   end
