d62cab4df1408b7dd2f86d9e8c7f9fd30ef81ac5
yangjunfeng <408290909@qq.com>
2015-01-11 21:36:56 +0800

    设置早中晚3个定时任务

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -2,6 +2,22 @@ require 'rufus-scheduler'
 
 scheduler = Rufus::Scheduler.new
 
-scheduler.at '20:40:00' do
-  puts "asldkfjlaksdjflkasdjlfk----"
+scheduler.at '21:40:00' do
+  p "------new day start----"
+  emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
+  GoodMorning.perform_async(emails)
 end
+
+scheduler.at '21:45:00' do
+  p "------noon-------"
+  emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
+  GoodNoon.perform_async(emails)
+end
+
+scheduler.at '21:50:00' do
+  p "------after noon------"
+  emails = TakeTaskPerson.execute(Time.now).map(&:email).join(",")
+  GoodAfterNoon.perform_async(emails)
+end
+
+
