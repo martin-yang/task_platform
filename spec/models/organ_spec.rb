fd3b878407ba48e336ca78775d6715239c035ce1
yangjunfeng <408290909@qq.com>
2015-02-03 07:15:16 +0800

    user don't repeat in organ

--- spec/models/organ_spec.rb
+++ spec/models/organ_spec.rb
@@ -11,4 +11,11 @@ describe Organ, :type => :model do
     expect(@organ.users.size).to eq(1)
     expect(@organ.users.first).to eq(@user)
   end
+
+  it "don't repeat to user" do
+    @organ.join_us @user
+    expect(@organ.users.size).to eq(1)
+    expect(@organ.join_us(@user)).to eq(false)
+    expect(@organ.users.size).to eq(1)
+  end
 end
