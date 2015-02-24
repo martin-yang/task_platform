71e586e6533569c58bfef8217d7bf041f56f8dee
yangjunfeng <408290909@qq.com>
2015-02-24 10:02:30 +0800

    build home_controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,9 @@
 class HomeController < ApplicationController
+  def index
+  	tasks = current_user.tasks
+    @total = tasks.count
+    @today = current_user.today_tasks.count
+    @pending = tasks.where(state: "new").count
+    @complete = tasks.where(state: "closed").count
+  end
 end
