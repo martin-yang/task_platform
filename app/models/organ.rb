e16784a0874b96929edff8cb7fa35b62174534a1
yangjunfeng <408290909@qq.com>
2015-03-15 20:59:16 +0800

    modify organ and actor relation;include create organ and after add default role in organ

--- app/models/organ.rb
+++ app/models/organ.rb
@@ -1,8 +1,11 @@
 class Organ < ActiveRecord::Base
+  include OrganRole
   validates :name, :context, presence: true
 
   has_and_belongs_to_many :users, class_name: "User"
-
+  has_many :actors, class_name: "Actor"
+  after_create :add_default_roles
+  
   def join_us user
     return false if self.users.include?(user)
     self.users << user
