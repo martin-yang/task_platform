c6d670c7c2a6a4e79432a7e5f4d65899af01a395
yangjunfeng <408290909@qq.com>
2015-03-15 20:33:11 +0800

    modify task num setting

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,7 +1,5 @@
-class Task < ActiveRecord::Base  
-  validates :name, :content, :user, presence: true
-  before_validation :build_number
-
+class Task < ActiveRecord::Base
+  validates :num, :name, :content, :user, presence: true
   belongs_to :date_record, class_name: "DateRecord"
   belongs_to :user, class_name: "User"
   after_create :create_date_record
@@ -13,16 +11,14 @@ class Task < ActiveRecord::Base
   end
 
   private
-  def build_number
-    unless self.num
-      now_date = Time.now
-      self.num, self.state = "#{now_date.year}#{now_date.to_i}", "new"
-    end
-  end
-
   def create_date_record
     now_date = Time.now
     date_record = DateRecord.find_or_create_by(year: now_date.year, month: now_date.month, day: now_date.day)
     self.update_attributes(date_record: date_record)
+  end
+
+  def self.build_number
+    now_date = Time.now
+    "#{now_date.year}#{now_date.to_i}"
   end
 end
--- spec/factories/task.rb
+++ spec/factories/task.rb
@@ -1,6 +1,8 @@
 FactoryGirl.define do
   factory :task, class: Task do
-  	name "task_name"
+    num Task.build_number
+    state "new"
+    name "task_name"
     content "test_task"
     association :user, factory: :user
   end
